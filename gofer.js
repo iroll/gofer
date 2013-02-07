4ca69da08419ee880e7fc355ac2f523c7dbed397
iroll <isaac.roll@gmail.com>
2013-02-06 17:20:34 -0700

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   gofer.js
    	renamed:    gofer.js -> launch.js
    
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   manifest.json
    	modified:   viewer.html

--- gofer.js
+++ gofer.js
@@ -1,4 +1,23 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('viewer.html', {
+//connect - calls TcpClient
+function connect(host, port, query) {
+  opensocket = new TcpClient(host, port);
+  opensocket.connect(function() {
+    opensocket.sendMessage(query);
+    opensocket.addResponseListener(function(response) {
+      var lines = response.split('\n');
+      var output = lines.join('<br/>');
+      document.getElementById('output').innerHTML = output;
+    });
   });
+}
+
+//Make the simple connection form in viewer work
+var button = document.getElementById('connect');
+button.addEventListener('click', function () {
+
+  var host = document.getElementById('host').value;
+  var port = parseInt(document.getElementById('port').value, 10);
+  var query = document.getElementById('query').value;
+  connect(host, port, query);
+
 });
