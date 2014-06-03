7029390bcdd33ef2870b3f0a8d7094b73dc274ab
iroll <isaac.roll@gmail.com>
2014-06-03 11:00:46 -0700

    apparently outerBounds requires chrome36

--- launch.js
+++ launch.js
@@ -1,6 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('viewer.html', {
-    outerBounds: {
+    id: "gofer",
+    bounds: {
       width: 800,
       height: 600
     }
